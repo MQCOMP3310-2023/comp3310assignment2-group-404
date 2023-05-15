83964741d37b0375c2fdd50596993a00903411d6
Taylan Ozdemir <taylan.ozdemir505@gmail.com>
2023-05-15 19:35:27 +1000

    Basic login screen with auth flow (auth.py)

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from project import create_app
+from project.auth import auth
 
 if __name__ == '__main__':
-  app = create_app()
-  app.run(host = '0.0.0.0', port = 8000, debug=True)
+    app = create_app()
+    app.register_blueprint(auth)  # Register the auth Blueprint
+    app.run(host='0.0.0.0', port=8000, debug=True)
