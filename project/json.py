b0f7cfc83f992730ef8b69a2b591004d7544143b
EthanP1111 <ethan.padget@students.mq.edu.au>
2023-05-27 14:38:42 +1000

    CodeQL Fixes

--- project/json.py
+++ project/json.py
@@ -9,15 +9,17 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    items = db.session.execute(text('select * from menu_item where restaurant_id = ' + str(restaurant_id)))
+    query = text('SELECT * FROM menu_item WHERE restaurant_id = :restaurant_id')
+    items = db.session.execute(query, {'restaurant_id': restaurant_id})
     items_list = [ i._asdict() for i in items ]
     return pyjs.dumps(items_list)
 
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    Menu_Item = db.session.execute(text('select * from menu_item where id = ' + str(menu_id) + ' limit 1'))
-    items_list = [ i._asdict() for i in Menu_Item ]
+    query = text('SELECT * FROM menu_item WHERE id = :menu_id LIMIT 1')
+    menu_item = db.session.execute(query, {'menu_id': menu_id})
+    items_list = [ i._asdict() for i in menu_item ]
     return pyjs.dumps(items_list)
 
 @json.route('/restaurant/JSON')
